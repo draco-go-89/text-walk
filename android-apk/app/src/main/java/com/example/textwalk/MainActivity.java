b062bf2483a677f03a0d1610bb90831c40292248
Dhrubo Basumatary <lubolabao509@gmail.com>
2026-08-18 22:51:07 +0530

    updated the app v 2.0

--- android-apk/app/src/main/java/com/example/textwalk/MainActivity.java
+++ android-apk/app/src/main/java/com/example/textwalk/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,6 +17,7 @@ import androidx.core.view.WindowInsetsControllerCompat;
 public class MainActivity extends AppCompatActivity {
 
     private static final String URL = "file:///android_asset/index.html";
+    private WebView webView;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -31,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        WebView webView = findViewById(R.id.webView);
+        webView = findViewById(R.id.webView);
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -45,17 +47,31 @@ public class MainActivity extends AppCompatActivity {
         // Optimization for smooth animation
         webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
 
+        // Add Javascript Interface
+        webView.addJavascriptInterface(new WebAppInterface(), "Android");
+
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(URL);
     }
 
     @Override
     public void onBackPressed() {
-        WebView webView = findViewById(R.id.webView);
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
+        // Instead of exiting, tell the webview to toggle settings or handle it
+        if (webView != null) {
+            webView.evaluateJavascript("toggleSettings()", null);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    /**
+     * Interface for Web Content to communicate with Android
+     */
+    public class WebAppInterface {
+        @JavascriptInterface
+        public void exitApp() {
+            finish();
+            System.exit(0);
         }
     }
 }
